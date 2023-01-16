d1557dec26e6e29610868cb6f242fa499fb76dbc
Swastik Gorai <swastikgorai@gmail.com>
2023-01-16 16:08:32 +0530

    fixed streamlit issue

--- start.py
+++ start.py
@@ -7,7 +7,7 @@ from generate import gen
 def main():
     st.title("Generate Image")
     st.markdown(
-        "This mini-app generates Tweets using OpenAI's GPT-3 based [Davinci model](https://beta.openai.com/docs/models/overview) for texts and [DALL·E](https://beta.openai.com/docs/guides/images) for images. You can find the code on [GitHub](https://github.com/kinosal/tweet) and the author on [Twitter](https://twitter.com/kinosal)."
+        "This mini-app generates Images using OpenAI's GPT-3 based model for Images."
     )
 
     topic = st.text_input(label="Describe your image", placeholder="Rick and Morty in Clockwork World")

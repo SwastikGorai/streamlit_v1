8a950b9067094a14cf8575ecf56c6c2f8f71b248
Swastik Gorai <swastikgorai@gmail.com>
2023-02-02 23:12:01 +0530

    improvement to ui

--- generate.py
+++ generate.py
@@ -9,14 +9,23 @@ def gen(topic):
     if not topic:
         st.session_state.text_error = "Please enter a topic"
         return
-    response = openai.Image.create(
-        prompt=topic,
-        n=1,
-        size="1024x1024"
-    )
-    image_url = response['data'][0]['url']
+    with st.spinner("Generating . . ."):
+        try:
+            response = openai.Image.create(
+                prompt=topic,
+                n=1,
+                size="512x512"
+            )
+            image_url = response['data'][0]['url']
+            st.image(image_url)
+        except openai.error.InvalidRequestError:
+            image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2MJanKV-BZsF_47xOBRAbkhLbrH56y5cRoA&usqp=CAU"
+            st.image(image_url)
+            st.caption('Uh oh. It seems that something is _not_ right. . . 😣😥')
 
-    st.image(image_url)
+
+
+
 
 
 
